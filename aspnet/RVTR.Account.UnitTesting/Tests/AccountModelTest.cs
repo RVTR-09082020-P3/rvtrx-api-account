93a6826c0af5a259d1e97c3013c1bf7f0b8d63a6
Alex Walker <atwalk14@gmail.com>
2020-10-28 14:37:04 -0400

    Makes model validation methods more robust

--- aspnet/RVTR.Account.UnitTesting/Tests/AccountModelTest.cs
+++ aspnet/RVTR.Account.UnitTesting/Tests/AccountModelTest.cs
@@ -16,6 +16,7 @@ namespace RVTR.Account.UnitTesting.Tests
           Id = 0,
           Address = new AddressModel(),
           Name = "name",
+          Email = "test@gmail.com",
           Payments = new List<PaymentModel>(),
           Profiles = new List<ProfileModel>()
         }
